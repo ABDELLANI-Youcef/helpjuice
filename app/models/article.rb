97983e89767c70f35b7c264754ccbdd5e207c103
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2022-12-01 09:04:34 +0100

    fix linter errors

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Article < ApplicationRecord
   validates_presence_of :title, :content, message: "can't be blank"
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyString" }
+    title { 'MyString' }
+    content { 'MyString' }
   end
 end
